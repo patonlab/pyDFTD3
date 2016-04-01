77e08a32ac97e1d55d7c63882b7a2fa88a8a0af9
Robert Paton <robertpaton@gmail.com>
2016-04-01 10:04:05 -0600

    Added pairwise and intermolecular functions

--- dftd3.py
+++ dftd3.py
@@ -26,7 +26,7 @@
 #    implemented.                                                     #
 #######################################################################
 #######  Written by:  Rob Paton and Kelvin Jackson ####################
-#######  Last modified:  Mar 20, 2013 #################################
+#######  Last modified:  Mar 20, 2016 #################################
 #######################################################################
 
 # Dependent on parameter file
@@ -217,12 +217,15 @@ c6ab = copyc6(max_elem, maxc)
 ## The periodic table...
 elm=['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr']
 
-## Published parameters for zero-damping optimized for different functionals
+## Published parameters (S6, RS6, S8) for zero-damping optimized for different functionals
 zero_parms = [["B2PLYP",	0.6400,	1.4270,	1.0220],["B97",	1.0000,	0.8920,	0.9090],["B3LYP",	1.0000,	1.2610,	1.7030],["BLYP",	1.0000,	1.0940,	1.6820],["PBE1PBE",	1.0000,	1.2870,	0.9280],["TPSSTPSS",	1.0000,	1.1660,	1.1050],["PBEPBE",	1.0000,	1.2170,	0.7220],["BP86",	1.0000,	1.1390,	1.6830],["BPBE",	1.0000,	1.0870,	2.0330],["B3PW91",	1.0000,	1.1760,	1.7750],["BMK",	1.0000,	1.9310,	2.1680],["CAM-B3LYP",	1.0000,	1.3780,	1.2170],["LC-wPBE",	1.0000,	1.3550,	1.2790],["M05",	1.0000,	1.3730,	0.5950],["M052X",	1.0000,	1.4170,	0.0000],["M06L",	1.0000,	1.5810,	0.0000],["M06",	1.0000,	1.3250,	0.0000],["M062X",	1.0000,	1.6190,	0.0000],["M06HF",	1.0000,	1.4460,	0.0000]]
+
+## Published parameters (S6, S8, a1, a2) for Becke-Johnson-damping optimized for different functionals
+bj_parms = [["B2PLYP",	0.6400,	0.9147,	0.3065,	5.0570],["B97",	1.0000,	2.2609,	0.5545,	3.2297],["B3LYP",	1.0000,	1.9889,	0.3981,	4.4211],["BLYP",	1.0000,	2.6996,	0.4298,	4.2359],["PBE1PBE",	1.0000,	1.2177,	0.4145,	4.8593],["TPSSTPSS",	1.0000,	1.9435,	0.4535,	4.4752],["PBEPBE",	1.0000,	0.7875,	0.4289,	4.4407],["BP86",	1.0000,	3.2822,	0.3946,	4.8516],["BPBE",	1.0000,	4.0728,	0.4567,	4.3908],["B3PW91",	1.0000,	2.8524,	0.4312,	4.4693],["BMK",	1.0000,	2.0860,	0.1940,	5.9197],["CAM-B3LYP",	1.0000,	2.0674,	0.3708,	5.4743],["LC-wPBE",	1.0000,	1.8541,	0.3919,	5.0897]]
 
 ## The computation of the D3 dispersion correction
 class calcD3:
-   def __init__(self, file, s6, rs6, s8, damp, abc, intermolecular):
+   def __init__(self, file, s6, rs6, s8, a1, a2, damp, abc, intermolecular, pairwise):
 
       ## Use ccParse to get the Cartesian coordinates from Gaussian input/output files
       if len(file.split(".com"))>1 or len(file.split(".gjf"))>1: fileData = getinData(file)
@@ -248,8 +251,6 @@ class calcD3:
       ## Names are pretty obvious...
       self.attractive_r6_vdw = 0.0; self.attractive_r8_vdw = 0.0; self.repulsive_abc = 0.0
 
-      #print "o  Using the following C6 cooefficients"
-      #print "   ID   Z    CN        C6"
       mxc=[0]
       for j in range(0,max_elem):
          mxc.append(0)
@@ -257,9 +258,7 @@ class calcD3:
             if atomtype[k].find(elements[j])>-1:
                for l in range(0,maxc):
                   if isinstance(c6ab[j][j][l][l], (list, tuple)):
-                     if c6ab[j][j][l][l][0]>0:
-                        #print "  ", atomtype[k],"  ", (j+1),"  ", c6ab[j][j][l][l][1],"  ", c6ab[j][j][l][l][0]
-                        mxc[j]=mxc[j]+1
+                     if c6ab[j][j][l][l][0]>0: mxc[j]=mxc[j]+1
                break
 
       ## Coordination number based on covalent radii
@@ -301,9 +300,14 @@ class calcD3:
             else: print "   WARNING: Damping parameters not specified and no functional could be read!\n"; sys.exit()
          else: print " manual parameters have been defined"
          print "   Zero-damping parameters:", "s6 =",s6, "rs6 =", rs6, "s8 =",s8
-      if abc == "off": print "   3-body term will not be calculated"
-      else: print "   Including the Axilrod-Teller-Muto 3-body dispersion term"
-      if intermolecular == "on": print "   Only computing intermolecular dispersion interactions! This is not the total D3-correction"
+     
+      if damp == "bj":
+         print "\n   D3-dispersion correction with Becke_Johnson damping:",
+         print "   WARNING: Not yet implemented!\n"; sys.exit()
+
+      if abc == "off": print "   3-body term will not be calculated\n"
+      else: print "   Including the Axilrod-Teller-Muto 3-body dispersion term\n"
+      if intermolecular == "on": print "   Only computing intermolecular dispersion interactions! This is not the total D3-correction\n"
 
       for j in range(0,natom):
          ## This could be used to 'switch off' dispersion between bonded or geminal atoms ##
@@ -314,7 +318,7 @@ class calcD3:
             if intermolecular == "on":
                if mols[j] == mols[k]:
                   scalefactor = 0
-                  print "   --- ignoring interactions between atoms",(j+1), "and", (k+1)
+                  print "   --- Ignoring interaction between atoms",(j+1), "and", (k+1)
 
             if scaling=="on" and hasattr(fileData,"BONDINDEX"):
                if fileData.BONDINDEX[j][k]==1: scalefactor = 0
@@ -324,7 +328,6 @@ class calcD3:
                           if fileData.BONDINDEX[j][l] != 0 and fileData.BONDINDEX[l][m]!=0 and fileData.BONDINDEX[k][m]!=0 and j!=m and k!=l and fileData.BONDINDEX[j][m]==0: scalefactor=1/1.2
 
             if k>j:
-               #print "  ", (j+1), (k+1),"  ", atomtype[j], atomtype[k],"  ", scalefactor
                ## Pythagoras in 3D to work out distance ##
                xdist = xco[j]-xco[k]; ydist = yco[j]-yco[k]; zdist = zco[j]-zco[k]
                totdist = math.pow(xdist,2)+math.pow(ydist,2)+math.pow(zdist,2)
@@ -340,18 +343,21 @@ class calcD3:
                C8jk = 3.0*C6jk*r2r4[atomA]*r2r4[atomB]
                C10jk=49.0/40.0 * math.pow(C8jk,2.0)/C6jk
 
-               dist=totdist/autoang
-               rr = r[atomA][atomB]/dist
-               tmp1 = rs6*rr
-               damp6 = 1/(1+6*math.pow(tmp1,alpha6))
-               tmp2 = rs8*rr
-               damp8 = 1/(1+6*math.pow(tmp2,alpha8))
+               # Evaluation of the attractive term dependent on R^-6 and R^-8
+               if damp == "zero":
+                  dist=totdist/autoang
+                  rr = r[atomA][atomB]/dist
+                  tmp1 = rs6*rr
+                  damp6 = 1/(1+6*math.pow(tmp1,alpha6))
+                  tmp2 = rs8*rr
+                  damp8 = 1/(1+6*math.pow(tmp2,alpha8))
 
-               # Evaluation of the attractive term dependent on R^-6
-               self.attractive_r6_term = -s6*C6jk*damp6/math.pow(dist,6)*autokcal*scalefactor
-
-               # Evaluation of the attractive term dependent on R^-8
-               self.attractive_r8_term = -s8*C8jk*damp8/math.pow(dist,8)*autokcal*scalefactor
+                  self.attractive_r6_term = -s6*C6jk*damp6/math.pow(dist,6)*autokcal*scalefactor
+                  self.attractive_r8_term = -s8*C8jk*damp8/math.pow(dist,8)*autokcal*scalefactor
+               
+                  if pairwise == "on" and scalefactor != 0:
+                     print "   --- Pairwise interaction between atoms", (j+1), "and", (k+1),": Edisp =", "%.6f" % (self.attractive_r6_term + self.attractive_r8_term), "kcal/mol"
+            
                self.attractive_r6_vdw = self.attractive_r6_vdw + self.attractive_r6_term
                self.attractive_r8_vdw = self.attractive_r8_vdw + self.attractive_r8_term
 
@@ -391,24 +397,27 @@ class calcD3:
 if __name__ == "__main__":
    # Takes arguments: (1) damping style, (2) s6, (3) rs6, (4) s8, (5) 3-body on/off, (6) input file(s)
    files = []
-   damp = "zero"; zero_s6 = 0.0; zero_rs6 = 0.0; zero_s8 = 0.0; abc_term = "off"; intermolecular = "off"
+   damp = "zero"; s6 = 0.0; rs6 = 0.0; s8 = 0.0; bj_a1 = 0.0; bj_a2 = 0.0; abc_term = "off"; intermolecular = "off"; pairwise = "off"
    if len(sys.argv) > 1:
       for i in range(1,len(sys.argv)):
          if sys.argv[i] == "-damp": damp = (sys.argv[i+1])
-         elif sys.argv[i] == "-s6": zero_s6 = float(sys.argv[i+1])
-         elif sys.argv[i] == "-rs6": zero_rs6 = float(sys.argv[i+1])
-         elif sys.argv[i] == "-s8": zero_s8 = float(sys.argv[i+1])
+         elif sys.argv[i] == "-s6": s6 = float(sys.argv[i+1])
+         elif sys.argv[i] == "-rs6": rs6 = float(sys.argv[i+1])
+         elif sys.argv[i] == "-s8": s8 = float(sys.argv[i+1])
+         elif sys.argv[i] == "-a1": bj_a1 = float(sys.argv[i+1])
+         elif sys.argv[i] == "-a2": bj_a2 = float(sys.argv[i+1])
          elif sys.argv[i] == "-3body": abc_term = (sys.argv[i+1])
-         elif sys.argv[i] == "-intermolecular": intermolecular = (sys.argv[i+1])
+         elif sys.argv[i] == "-im": intermolecular = (sys.argv[i+1])
+         elif sys.argv[i] == "-pw": pairwise = (sys.argv[i+1])
          else:
             if len(sys.argv[i].split(".")) > 1:
                if sys.argv[i].split(".")[1] == "out" or sys.argv[i].split(".")[1] == "log" or sys.argv[i].split(".")[1] == "com" or sys.argv[i].split(".")[1] == "gjf":
                   files.append(sys.argv[i])
 
-   else: print "\nWrong number of arguments used. Correct format: dftd3.py (-damp zero/bj) (-s6 val) (-rs6 val) (-s8 val) (-intermolecular on/off) file(s)\n"; sys.exit()
+   else: print "\nWrong number of arguments used. Correct format: dftd3.py (-damp zero/bj) (-s6 val) (-rs6 val) (-s8 val) (-a1 val) (-a2 val) (-im on/off) (-pw on/off)file(s)\n"; sys.exit()
 
    for file in files:
-      fileD3 = calcD3(file, zero_s6, zero_rs6, zero_s8, damp, abc_term, intermolecular)
+      fileD3 = calcD3(file, s6, rs6, s8, bj_a1, bj_a2, damp, abc_term, intermolecular, pairwise)
       attractive_r6_vdw = fileD3.attractive_r6_vdw/autokcal
       attractive_r8_vdw = fileD3.attractive_r8_vdw/autokcal
 
